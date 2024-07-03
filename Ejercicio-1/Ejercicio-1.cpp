95d143cb1ee8957412c19976cfcedefc8501fda5
EzeNotebook <ezgarcia@frba.utn.edu.ar>
2024-07-03 20:26:36 -0300

    empezando con el ejercicio

---
+++ Ejercicio-1/Ejercicio-1.cpp
@@ -0,0 +1,26 @@
+/*
+Un consorcio desea controlar la cobranza de las expensas en un edificio de 5 pisos
+donde existen 15 departamentos en cada piso.
+El valor de las expensas es de $ 3000.
+Para registrar la cobranza se ingresa el n�mero de piso (1 a 5)
+y el n�mero de departamento (correlativo de 1 a 15 en cada piso).
+El ingreso finaliza con un piso igual a 0.
+Al finalizar mostrar el dinero total recaudado y
+una tabla con una X indicando aquellos departamentos
+deudores de la siguiente manera:
+TOTAL RECAUDADO
+DEPARTAMENTOS DEUDORES
+PISO    DPTO 1 DPTO2 � DPTO 15
+PISO 1    X
+PISO 2    X     X
+PISO 3    X
+PISO 4    X     X
+PISO 5    X
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
